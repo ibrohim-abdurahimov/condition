ca8d04adcb4902dc24e123c8d3e6ae72cf1328b6
ibrohim-abdurahimov <ibr20040322@gmail.com>
2024-08-29 09:13:01 +0500

    Condition

---
+++ app.js
@@ -0,0 +1,138 @@
+
+{
+    // console.log("masala-1")
+    // let num =1
+    // if(num>=0){
+    //     console.log(num + 1)
+    // }else{
+    //     console.log(num)
+    // }
+}
+
+
+{
+    // consol.log("masala-2")
+    // let num =-5
+    // if(num>=0){
+    //     console.log(num+1)
+    // }else{
+    //     console.log(num-2)
+    // }
+}
+
+
+{
+    // consol.log("masala-3")
+    // let num =5
+    // if(num>0){
+    //     console.log(num+1)
+    // }if(num<0){
+    //     console.log(num-2)
+    // }else{
+    //     console.log(num=10)
+    // }
+}
+
+
+{
+    // console.log("masala-4")
+    // let numFirst =5
+    // let numSecond =1
+    // let numThirth = -5
+    // let musbat = 0
+    // if(numFirst>0){
+    //     musbat++
+    // }
+    // if(numSecond>0){
+    //     musbat++
+    // }
+    // if(numThirth>0){
+    //     musbat++
+    // }
+    // console.log(`musbatlar ${musbat}`)
+    
+    
+}
+
+{
+    // console.log("masala-5")
+    // let numFirst =5
+    // let numSecond =-1
+    // let numThirth = -5
+    // let musbat = 0
+    // let manfiy = 0
+    // if(numFirst>=0){
+    //     musbat++
+    // }else{
+    //     manfiy++
+    // }
+    // if(numSecond>=0){
+    //     musbat++
+    // }else{
+    //     manfiy++
+    // }
+    // if(numThirth>=0){
+    //     musbat++
+    // }else{
+    //     manfiy++
+    // }
+    // console.log(`musbatlar: ${musbat} ; manfiylar: ${manfiy} `)
+}
+
+{
+    // console.log("masala-6")
+    // let numFirst = 5
+    // let numSecond = 78
+    // if(numFirst>numSecond){
+    //     console.log(`katta son: ${numFirst}`)
+    // }else{
+    //     console.log(`katta son: ${numSecond}`)
+    // }
+}
+
+{
+    // console.log("masala-7")
+    // let numFirst = 8
+    // let numSecond= 4
+    // if(numFirst<numSecond){
+    //     console.log(`kichik sonning tartib raqami : ${1}`)
+    // }else{
+    //     console.log(`kichik sonning tartib raqami : ${2}`)
+    // }
+}
+
+{
+    // console.log("masala-8")
+    // let numFirst = 33
+    // let numSecond = 45
+    // if(numFirst<numSecond){
+    //     console.log(`kichik son: ${numFirst} ; katta son: ${numSecond}`)
+    // }else{
+    //     console.log(`kichik son: ${numSecond} ; katta son: ${numFirst}`)
+    // }
+}
+
+{
+    // console.log("masala-9")
+    // let a = 9
+    // let b =4
+    // let f
+    // if(a>b){
+    //     c=b;
+    //     b=a;
+    //     a=c;
+    //     console.log(`kichik son ${a} ; katta son ${b}`)
+
+    // }
+}
+
+{
+    // console.log("masala-10")
+    // let a = 20
+    // let b = 15
+    // if(a===b){
+    //     console.log(`a ni qiymati ${a}; b ni qiymati ${b} ; a=0; b=0`)
+    // }else{
+    //     console.log(a+b)
+    // }
+}
